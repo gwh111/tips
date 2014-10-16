85f72010f2245e34089175703320223164c9672a
apple <apple@apples-Mac-mini.local>
2014-10-16 16:43:10 +0800

    add

---
+++ Tips&Notes/tttttViewController.m
@@ -0,0 +1,49 @@
+//
+//  tttttViewController.m
+//  Tips&Notes
+//
+//  Created by apple on 16/10/14.
+//  Copyright (c) 2014年 ___GWH___. All rights reserved.
+//
+
+#import "tttttViewController.h"
+
+@interface tttttViewController ()
+
+@end
+
+@implementation tttttViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
